4f530ae10750819b592f493138db10093422cb2b
Michael DeLally <michael.d@outdoornetwork.com>
2020-01-02 16:04:05 -0500

    Initial commit with splash screen

---
+++ src/layouts/Default.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="layout">
+    <slot />
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Changa+One&display=swap");
+
+body,
+html {
+  height: 100%;
+}
+
+body {
+  font-family: "Changa One", sans-serif;
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+  background: #222;
+  color: white;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  height: 100vh;
+}
+
+.layout {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,54 @@
+<template>
+  <Layout>
+    <g-image src="~/assets/img/logo.svg" class="logo"></g-image>
+
+    <h1>NEW SITE COMING SOON...</h1>
+
+    <div class="socials">
+      <a href="https://twitter.com/thought_reactor">TWITTER</a>
+      <a href="https://instagram.com/thoughtreactor">INSTAGRAM</a>
+      <a href="https://twitch.tv/thoughtreactordev">TWITCH</a>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Coming Soon..."
+  }
+};
+</script>
+
+<style>
+h1 {
+  text-align: center;
+  font-weight: 400;
+}
+
+.logo {
+  width: 100%;
+  max-width: 900px;
+}
+
+.socials {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  max-width: 600px;
+  padding: 2em;
+}
+
+.socials > a {
+  color: #da4917;
+  font-size: 1.2rem;
+  text-decoration: none;
+  padding: 0.5em;
+}
+
+.socials > a:hover {
+  color: white;
+}
+</style>
