cce56e20dc368303aa377a967022eda99ecdb5fb
Michael DeLally <michael.d@outdoornetwork.com>
2020-01-14 11:19:47 -0500

    Google analytics and sitemap plugins

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,19 @@
-// This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
   siteName: "Thought Reactor",
-  plugins: []
+  plugins: [
+    {
+      use: "@gridsome/plugin-google-analytics",
+      options: {
+        id: "UA-47036341-4"
+      }
+    },
+    {
+      use: "@gridsome/plugin-sitemap",
+      options: {
+        cacheTime: 600000, // default
+        exclude: [],
+        config: {}
+      }
+    }
+  ]
 };
