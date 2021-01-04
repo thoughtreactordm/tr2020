33444a439e6d95314b660d8921279fa59a048ed5
Michael DeLally <michael.d@outdoornetwork.com>
2021-01-04 09:19:03 -0500

    Update launch date in IGEO showcase

--- src/components/partials/game-showcases/IgeoShowcase.vue
+++ src/components/partials/game-showcases/IgeoShowcase.vue
@@ -1,8 +1,14 @@
 <template>
   <section class="showcase py-16 lg:py-32 my-16 w-full border-t-4">
-    <div class="container mx-auto md:px-16 flex flex-col items-center lg:text-center text-xl">
+    <div
+      class="container mx-auto md:px-16 flex flex-col items-center lg:text-center text-xl"
+    >
       <g-link href="https://igeogame.com" title="IGEO DX Logo">
-        <g-image src="~/assets/img/igeo_logo.png" class="logo mb-4" alt="Thought Reactor Logo"></g-image>
+        <g-image
+          src="~/assets/img/igeo_logo.png"
+          class="logo mb-4"
+          alt="Thought Reactor Logo"
+        ></g-image>
       </g-link>
 
       <div class="info flex flex-col lg:flex-row lg:items-center">
@@ -10,14 +16,14 @@
           <vimeo id="441158036"></vimeo>
         </div>
         <div class="info-body lg:p-8 lg:w-1/2">
-          <h2
-            class="xs:text-3xl sm:text-4xl text-shadow leading-tight"
-          >Coming to Steam December 2020!</h2>
+          <h2 class="xs:text-3xl sm:text-4xl text-shadow leading-tight">
+            Coming to Steam May 2021!
+          </h2>
           <p class="lg:text-xl my-6">
             Casual in it's approach but punishing in it's execution, IGEO is a
-            Sokoban-style puzzler. Mechanically simple but
-            challenging, players will push and combine like-shapes to solve
-            increasingly complex spatial and logic puzzles.
+            Sokoban-style puzzler. Mechanically simple but challenging, players
+            will push and combine like-shapes to solve increasingly complex
+            spatial and logic puzzles.
           </p>
 
           <iframe
@@ -31,7 +37,8 @@
             to="https://thoughtreactor.itch.io/igeo-dx"
             :icon="['fab', 'itch-io']"
             utils="text-white"
-          >Download Alpha on Itch.io</tr-button-link>
+            >Download Alpha on Itch.io</tr-button-link
+          >
         </div>
       </div>
     </div>
